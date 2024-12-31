df72e1706812f5e6b9db98d84a4ec8b54e12a55a
Soma Satori <112419029+soma-satoro@users.noreply.github.com>
2024-12-30 16:38:29 -0800

    Delete world - Copy directory

--- world - Copy/jobs/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class JobsConfig(AppConfig):
-    name = 'world.jobs'
-    verbose_name = "Jobs System"
--- world - Copy/requests/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class MailConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'world.requests'
-
-    def ready(self):
-        import world.requests.signals
